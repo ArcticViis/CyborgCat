546f25e0e79459368c6972266b32933f3ca23d63
Markus Selin <h4192@student.jamk.fi>
2016-02-21 23:36:22 +0200

    Created the repo

---
+++ Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Werecat
+{
+    public class PlayerCameraController : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector3 cameraOffset = Vector3.zero;
+        [SerializeField]
+        private float lookSensitivity = 3f;
+        public float LookSensitivity
+        {
+            get { return lookSensitivity; }
+        }
+        [SerializeField]
+        private float lookMinMAx = 85f;
+        [SerializeField]
+        private Transform pivot;
+        [SerializeField]
+        private float sweepr = 1f;
+
+        private Camera cam;
+        private Rigidbody rb;
+
+
+        private float cameraXRotation = 0f;
+        // Use this for initialization
+        void Start()
+        {
+            cam = Camera.main;
+            rb = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void LateUpdate()
+        {
+            UpdateCamera();
+            //CameraCollision();
+        }
+        void UpdateCamera()
+        {
+            cameraXRotation = Mathf.Clamp(cameraXRotation - Input.GetAxisRaw("Mouse Y") * lookSensitivity, -lookMinMAx, lookMinMAx);
+
+            pivot.localPosition = new Vector3(cameraOffset.x, cameraOffset.y, 0);
+            
+            pivot.localEulerAngles = new Vector3(cameraXRotation, pivot.localEulerAngles.y, 0);
+            cam.transform.position = pivot.TransformPoint(Vector3.forward * cameraOffset.z);
+            cam.transform.LookAt(pivot);
+        }
+
+        void CameraCollision()
+        {
+            float _distance = cameraOffset.z;
+            RaycastHit _hit;
+            if(cam.GetComponent<Rigidbody>().SweepTest(cam.transform.forward * -1f, out _hit, 2f))
+            {
+                Debug.Log("sweep");
+                _distance += sweepr * Time.deltaTime;
+                _distance = Mathf.Clamp(_distance, -5f, 0f);
+            }
+            if (cam.GetComponent<Rigidbody>().SweepTest(cam.transform.forward * -2f, out _hit, 2f))
+            { Debug.Log("sweep2"); }
+            else
+            {
+                Debug.Log("back");
+                _distance -= sweepr* Time.deltaTime ;
+                _distance = Mathf.Clamp(_distance, -5f, 0f);
+            }
+            cameraOffset.z = _distance;
+
+
+
+        }
+    }
+}
+
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Werecat
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(PlayerCameraController))]
+    public class PlayerMotor : MonoBehaviour
+    {
+
+        [SerializeField]
+        private float movementSpeed = 5f;
+        [SerializeField]
+        private float jumpPower = 5f;
+        private float looksens;
+
+
+        private PlayerCameraController pcc;
+        private Rigidbody rb;
+
+        //public Vector3 pivot = Vector3.zero;
+        #region Inputs
+        private float forwardScale;
+        private float rightScale;
+        #endregion
+
+        // Use this for initialization
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            pcc = GetComponent<PlayerCameraController>();
+            looksens = pcc.LookSensitivity;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            UpdateInput();
+        }
+        void FixedUpdate()
+        {
+
+            Movement();
+            Turning();
+
+        }
+
+        void LateUpdate()
+        {
+        }
+        void UpdateInput()
+        {
+            forwardScale = Input.GetAxisRaw("Vertical");
+            rightScale = Input.GetAxisRaw("Horizontal");
+        }
+        void Movement()
+        {
+            Vector3 _forward = forwardScale * transform.forward;
+            Vector3 _right = rightScale * transform.right;
+            Vector3 _movement = (_forward + _right).normalized * movementSpeed;
+
+            rb.MovePosition(transform.position + _movement * Time.fixedDeltaTime);
+        }
+
+        void Turning()
+        {
+            float _rotationY = Input.GetAxisRaw("Mouse X");
+            transform.Rotate(new Vector3(0, _rotationY * looksens, 0));
+        }
+    }
+}
+
