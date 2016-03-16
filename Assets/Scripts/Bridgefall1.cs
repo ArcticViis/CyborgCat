af06b4c37cc2d5ccb5ed729b12cc6ac8d3bc2907
Niko Kangas <H8416@LABRANET.JAMK.FI>
2016-03-16 15:52:21 +0200

    Breaking bridges, making shit fall and maybe even have a shit related song in there....

---
+++ Assets/Scripts/Bridgefall1.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bridgefall1 : MonoBehaviour {
+
+
+	public AudioSource audioSource;
+	public Animator[] animator;
+	private bool kill = false;
+
+
+	void OnTriggerEnter(Collider coll)
+	{
+		
+
+		if(coll.gameObject.tag == "Player"){
+			Debug.Log("trying shit");
+			audioSource.PlayOneShot(audioSource.clip, 1.0f);
+
+			foreach (Animator _animator in animator) {
+				_animator.SetTrigger ("Playerenter1");
+				Debug.Log("Dropping shit");
+			}
+			for( int i = 0; i >= animator.Length; i++){
+				animator[i].SetTrigger ("Playerenter1");
+				Debug.Log("Dropping shit");
+			}
+				
+			kill = true;
+
+		}
+	}
+
+	void OnTriggerExit(Collider coll)
+	{
+		if (coll.gameObject.tag == "Player") {
+			Destroy (gameObject);
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		/*if (kill && !audioSource.isPlaying) {
+			Destroy (gameObject);
+		}*/
+
+	}
+}
