5a8546b01196f22d3a5054912ea04f7a5eab252f
Miro Paananen <H4226@Labranet.jamk.fi>
2016-03-23 15:32:55 +0200

    added muijaanimations

---
+++ Assets/movement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	public Animator ac;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
